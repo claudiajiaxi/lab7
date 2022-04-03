faac5534c31052d8e4ab5fc6d2bf8edf9ebedfbd
claudiajiaxi <claudiaxi0625@gmail.com>
2022-04-02 22:29:05 -0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function init(){
+//add your javascrip between these two lines of code
+
+  function clickAlertMe(){
+    var text = document.getElementById("entryinput").value;
+    alert("XI JIA: " + text);
+    document.getElementById("textoutput").innerHTML = text
+  }
+  var entrybtn = document.getElementById("entrybutton");
+  entrybtn.addEventListener('click',clickAlertMe);
+
+}
+window.addEventListener('load', init);
